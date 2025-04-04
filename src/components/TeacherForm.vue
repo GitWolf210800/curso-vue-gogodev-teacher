737b7aa8dec720fe9ef32d0d8639bbe2e4d2b052
GitWolf210800 <ezad821q@hotmail.com>
2025-04-04 18:25:07 -0300

    probamos las referencias en un formulario

---
+++ src/components/TeacherForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <h3>Añadir Profesor</h3>
+        <div><label>Nombre:</label> <input type="text" v-model="teacher.teacherName"></div>
+        <div><label>Apellido:</label> <input type="text" v-model="teacher.surname"></div>
+        <div><label>DNI / NIF:</label> <input type="text" v-model="teacher.dni"></div>
+        <div><label>Materias:</label> <input type="text" v-model="subjects"></div>
+        <input type="checkbox" v-model="teacher.doc">Documentación Entregada
+        <button>Agregar</button>
+    </section>
+        <h3>Listado de Profesores</h3>
+    <section>
+
+    </section>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref } from 'vue';
+
+interface ITeacher {
+    teacherName: string,
+    surname: string,
+    dni: string,
+    subjects: Array<string>,
+    doc: boolean
+}
+
+let teacher:Ref<ITeacher> = ref({
+    teacherName: '',
+    surname: '',
+    dni: '',
+    subjects: [],
+    doc: false
+})
+
+let teachers:Ref<Array<ITeacher>> = ref([]);
+
+let subjects:Ref<string> = ref('');
+</script>
+
+<style scoped>
+</style>
