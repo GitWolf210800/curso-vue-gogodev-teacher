f56499cfc856999a980f86fe2a094505655c6761
GitWolf210800 <ezad821q@hotmail.com>
2025-04-06 16:22:19 -0300

    probando v-for

--- src/components/TeacherForm.vue
+++ src/components/TeacherForm.vue
@@ -4,9 +4,14 @@
         <div><label>Nombre:</label> <input type="text" v-model="teacher.teacherName"></div>
         <div><label>Apellido:</label> <input type="text" v-model="teacher.surname"></div>
         <div><label>DNI / NIF:</label> <input type="text" v-model="teacher.dni"></div>
-        <div><label>Materias:</label> <input type="text" v-model="subjects"></div>
+        <div><label>Materias:</label> <input type="text" v-model="subject"></div>
+        <div>
+            <ul>
+                <li v-for="(elm, index) in teacher.subjects" :key="index">{{ elm }}</li>
+            </ul>
+        </div>
         <input type="checkbox" v-model="teacher.doc">Documentación Entregada
-        <button>Agregar</button>
+        <button @click="handleSubject">Agregar</button>
     </section>
         <h3>Listado de Profesores</h3>
     <section>
@@ -20,7 +25,7 @@ import { Ref, ref } from 'vue';
 interface ITeacher {
     teacherName: string,
     surname: string,
-    dni: string,
+    dni: string, 
     subjects: Array<string>,
     doc: boolean
 }
@@ -35,7 +40,11 @@ let teacher:Ref<ITeacher> = ref({
 
 let teachers:Ref<Array<ITeacher>> = ref([]);
 
-let subjects:Ref<string> = ref('');
+let subject:Ref<string> = ref('');
+
+const handleSubject = () => {
+    teacher.value.subjects.push(subject.value)
+};
 </script>
 
 <style scoped>
